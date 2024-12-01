79fb7ed1fb5a92d10073f5a18b08e8e25a7a99fb
pawel <pawel@polczynski.com>
2024-12-01 20:42:36 +0100

    moved the execution of the task to __main__.py so the task code is clean and separated

---
+++ d01/__main__.py
@@ -0,0 +1,17 @@
+from d01.historian_hysteria import HistorianHysteria
+
+if __name__ == '__main__':
+    list_a = []
+    list_b = []
+    split_characters = "   "
+    with open("data", "r") as data_file:
+        for line in data_file:
+            values = line.rstrip().split(split_characters)
+            list_a.append(int(values[0]))
+            list_b.append(int(values[1]))
+    if len(list_a) == len(list_b):
+        hh = HistorianHysteria(list_a, list_b)
+        print("What is the total distance between your lists?")
+        print(hh.get_list_distance())
+        print("What is their similarity score?")
+        print(hh.get_list_similarity())
--- d01/historian_hysteria.py
+++ d01/historian_hysteria.py
@@ -12,20 +12,3 @@ class HistorianHysteria:
     def get_list_similarity(self) -> int:
         cnt_list_b = Counter(self._list_b)
         return sum([num * cnt_list_b.get(num, 0) for num in self._list_a])
-
-
-if __name__ == '__main__':
-    l1 = []
-    l2 = []
-    split_characters = "   "
-    with open("data", "r") as data_file:
-        for line in data_file:
-            values = line.rstrip().split(split_characters)
-            l1.append(int(values[0]))
-            l2.append(int(values[1]))
-    if len(l1) == len(l2):
-        hh = HistorianHysteria(l1, l2)
-        result_a = hh.get_list_distance()
-        print(result_a)
-        result_b = hh.get_list_similarity()
-        print(result_b)
