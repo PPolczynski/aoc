52a89592f916ff837720ccc9122bb3aaad3df1f5
pawel <pawel@polczynski.com>
2024-12-01 21:05:34 +0100

    removed wrong comments

--- d01/historian_hysteria_test.py
+++ d01/historian_hysteria_test.py
@@ -9,14 +9,14 @@ class MyTestCase(unittest.TestCase):
         l2 = [4, 3, 5, 3, 9, 3]
         hysteria = HistorianHysteria(l1, l2)
         distance = hysteria.get_list_distance()
-        self.assertEqual(distance, 11)  # add assertion here
+        self.assertEqual(distance, 11)
 
     def test_example_similarity(self):
         l1 = [3, 4, 2, 1, 3, 3]
         l2 = [4, 3, 5, 3, 9, 3]
         hysteria = HistorianHysteria(l1, l2)
         similarity = hysteria.get_list_similarity()
-        self.assertEqual(similarity, 31)  # add assertion here
+        self.assertEqual(similarity, 31)
 
 
 if __name__ == '__main__':
