88741bd802519301524052b4f9e957371386b8c5
pawel <pawel@polczynski.com>
2024-12-02 15:41:19 +0100

    refactor day one tests only

--- d01/historian_hysteria_test.py
+++ d01/historian_hysteria_test.py
@@ -2,19 +2,17 @@ import unittest
 
 from d01.historian_hysteria import HistorianHysteria
 
+_list_a = [3, 4, 2, 1, 3, 3]
+_list_b = [4, 3, 5, 3, 9, 3]
 
-class MyTestCase(unittest.TestCase):
+class HistorianHysteriaTestCase(unittest.TestCase):
     def test_example_distance(self):
-        list_a = [3, 4, 2, 1, 3, 3]
-        list_b = [4, 3, 5, 3, 9, 3]
-        historian_hysteria = HistorianHysteria(list_a, list_b)
+        historian_hysteria = HistorianHysteria(_list_a, _list_b)
         distance = historian_hysteria.get_list_distance()
         self.assertEqual(distance, 11)
 
     def test_example_similarity(self):
-        list_a = [3, 4, 2, 1, 3, 3]
-        list_b = [4, 3, 5, 3, 9, 3]
-        historian_hysteria = HistorianHysteria(list_a, list_b)
+        historian_hysteria = HistorianHysteria(_list_a, _list_b)
         similarity = historian_hysteria.get_list_similarity()
         self.assertEqual(similarity, 31)
 
