6805d9db1b4f04a2e85b202318c3ced8ed9b32c5
pawel <pawel@polczynski.com>
2024-12-07 13:28:25 +0100

    day 7 2024 added missing type hints

--- y2024/d07/bridge_repair.py
+++ y2024/d07/bridge_repair.py
@@ -4,11 +4,11 @@ def concat(x: int, y: int) -> int:
 class BridgeRepair:
 
     @staticmethod
-    def get_sum_valid_targets(data: list[tuple[int, list[int]]]):
+    def get_sum_valid_targets(data: list[tuple[int, list[int]]]) -> int:
         return sum([target if BridgeRepair.is_valid(target, values) else 0 for target, values in data])
 
     @staticmethod
-    def is_valid(target: int, values: list[int]):
+    def is_valid(target: int, values: list[int]) -> int:
         return BridgeRepair._is_valid(values[0], 1, target, values)
 
     @staticmethod
@@ -22,12 +22,12 @@ class BridgeRepair:
                     or BridgeRepair._is_valid(current_value * values[idx], idx + 1, target, values))
 
     @staticmethod
-    def get_sum_valid_targets_with_concat(data: list[tuple[int, list[int]]]):
+    def get_sum_valid_targets_with_concat(data: list[tuple[int, list[int]]]) -> int:
         return sum([target if BridgeRepair.is_valid_with_concat(target, values) else 0
                     for target, values in data])
 
     @staticmethod
-    def is_valid_with_concat(target: int, values: list[int]):
+    def is_valid_with_concat(target: int, values: list[int]) -> int:
         return BridgeRepair._is_valid_with_concat(values[0], 1, target, values)
 
     @staticmethod
