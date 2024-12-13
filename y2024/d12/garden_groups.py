188fd50a0117e8248bc6263e01d994fc87d25f1c
pawel <pawel@polczynski.com>
2024-12-13 09:50:29 +0100

    day 12 2024 part 2

--- y2024/d12/garden_groups.py
+++ y2024/d12/garden_groups.py
@@ -1,12 +1,13 @@
 from utils.matrix import Matrix
 
 _directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+_edge_offset = [(0.5, -0.5), (0.5, 0.5), (-0.5, 0.5), (-0.5, -0.5)]
 
 class GardenGroups:
     def __init__(self, garden: list[str]):
         self.garden = Matrix(garden)
 
-    def _get_regions_with_perimeter(self) -> list[tuple[list[tuple[int, int]], int]]:
+    def _get_regions_with_perimeter_length(self) -> list[tuple[list[tuple[int, int]], int]]:
         regions = []
         visited = set()
         for crop, coordinate in self.garden:
@@ -18,8 +19,47 @@ class GardenGroups:
         return regions
 
     def get_fencing_cost(self) -> int:
-        regions = self._get_regions_with_perimeter()
+        regions = self._get_regions_with_perimeter_length()
         return sum(len(region) * perimeter for region, perimeter in regions)
+
+    def get_fencing_cost_bulk(self) -> int:
+        regions = self._get_regions_with_perimeter_length()
+        return sum(len(region) * self._get_sides_count(region) for region, _ in regions)
+
+    @staticmethod
+    def _get_sides_count(region: list[tuple[int, int]]) -> int:
+        edges = set()
+        # get all four edges of every point
+        for x, y in region:
+            for dx, dy in _edge_offset:
+                edges.add((x + dx, y + dy))
+        sides = 0
+        for x, y in edges:
+            edges_in_region = [1 if (x + dx, dy + y) in region else 0 for dx, dy in _edge_offset]
+            neighbours_count = sum(edges_in_region)
+            if neighbours_count == 4:
+                '''
+                NOT an edge
+                A || A
+                = +  =
+                A || A
+                '''
+                continue
+            elif neighbours_count == 1 or neighbours_count == 3:
+                '''
+                1)  A || .  3) A || .
+                    = +  =     = +  =
+                    . || .     A || A
+                '''
+                sides += 1
+            elif neighbours_count == 2 and edges_in_region in [[1, 0, 1, 0], [0, 1, 0, 1]] :
+                '''
+                THIS    A || . or . || A  NOT THIS  . || .  or  A || A  or  A || . or . || A 
+                 ->     = +  =    = +  =     ->     = +  =      = +  =      = +  =    = +  =
+                        . || A    A || .            A || A      . || .      A || .    . || A
+                '''
+                sides += 2
+        return sides
 
     def _get_region(self, crop: str, coordinate: tuple[int, int], region: set) -> int:
         if coordinate in region:
--- y2024/d12/garden_groups_test.py
+++ y2024/d12/garden_groups_test.py
@@ -41,6 +41,133 @@ class MyTestCase(unittest.TestCase):\
         garden_groups = GardenGroups(garden)
         self.assertEqual(1930, garden_groups.get_fencing_cost())
 
+    def test_get_fencing_cost_bulk_example1(self):
+        garden = [
+            "AAAA",
+            "BBCD",
+            "BBCC",
+            "EEEC"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(80, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example2(self):
+        garden = [
+            "OOOOO",
+            "OXOXO",
+            "OOOOO",
+            "OXOXO",
+            "OOOOO"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(436, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example3(self):
+        garden = [
+            "EEEEE",
+            "EXXXX",
+            "EEEEE",
+            "EXXXX",
+            "EEEEE"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(236, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example4(self):
+        garden = [
+            "AAAAAA",
+            "AAABBA",
+            "AAABBA",
+            "ABBAAA",
+            "ABBAAA",
+            "AAAAAA"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(368, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example5(self):
+        garden = [
+            "RRRRIICCFF",
+            "RRRRIICCCF",
+            "VVRRRCCFFF",
+            "VVRCCCJFFF",
+            "VVVVCJJCFE",
+            "VVIVCCJJEE",
+            "VVIIICJJEE",
+            "MIIIIIJJEE",
+            "MIIISIJEEE",
+            "MMMISSJEEE"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(1206, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example6(self):
+        garden = [
+            "AAAEAAAAAA",
+            "FFAEAADAAA",
+            "FFAAAADACA",
+            "FFAABAAAAB",
+            "FFABBBABBB",
+            "FAAAABBBBB",
+            "FAGGABBBBB",
+            "FAGAABBBBB"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(1992, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example7(self):
+        garden = [
+            "LDDDDDDXXX",
+            "LLLDDVDXXX",
+            "LLLDDDXXXX"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(250, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example8(self):
+        garden = [
+            "BBBBBC",
+            "BAAABC",
+            "BABABC",
+            "BAABBB",
+            "BABABC",
+            "BAAABC"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(492, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example9(self):
+        garden = [
+            "AAAAA",
+            "ABABA",
+            "ABBBA",
+            "ABABA"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(232, garden_groups.get_fencing_cost_bulk())
+
+    def test_get_fencing_cost_bulk_example10(self):
+        garden = [
+            "----",
+            "-OOO",
+            "-O-O",
+            "OO-O",
+            "-OOO"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(180, garden_groups.get_fencing_cost_bulk())
+
+
+    def test_get_fencing_cost_bulk_example11(self):
+        garden = [
+            "----",
+            "OOO-",
+            "O-O-",
+            "O-OO",
+            "OOO-"
+        ]
+        garden_groups = GardenGroups(garden)
+        self.assertEqual(180, garden_groups.get_fencing_cost_bulk())
 
 if __name__ == '__main__':
     unittest.main()
