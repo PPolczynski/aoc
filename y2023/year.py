96501ae6103e9d7e4932793ea0b336c855876480
pawel <pawel@polczynski.com>
2026-02-18 00:13:38 +0100

    6 2023 refactor

--- y2023/year.py
+++ y2023/year.py
@@ -4,9 +4,11 @@ from .d02 import solution as day2
 from .d03 import solution as day3
 from .d04 import solution as day4
 from .d05 import solution as day5
+from .d06 import solution as day6
 
 event = Event([day1.solution,
                day2.solution,
                day3.solution,
                day4.solution,
-               day5.solution])
+               day5.solution,
+               day6.solution])
