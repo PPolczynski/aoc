2c707cda67cac782152e1bc9bcfdfbebaf5f9c24
pawel <pawel@polczynski.com>
2026-02-19 20:16:20 +0100

    add path parameter

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import Optional
 import registration
 from puzzle import Solver, Client
 
+
 def get_config_value(key: str, arg_value: Optional[str] = None) -> Optional[str]:
     """Consolidated configuration lookup: Args > Env > config.ini > .env"""
     if arg_value:
@@ -14,8 +15,9 @@ def get_config_value(key: str, arg_value: Optional[str] = None) -> Optional[str]
     if config.read("config.ini") and "AOC" in config:
         if key in config["AOC"]:
             return config["AOC"][key]
-            
+
     return None
+
 
 def main():
     parser = argparse.ArgumentParser(description="Advent of Code runner")
@@ -23,17 +25,19 @@ def main():
     parser.add_argument("-d", "--day", help="Day to run")
     parser.add_argument("-token", "--token", help="AOC session token")
     parser.add_argument("-refresh", "--refresh", action="store_true", help="Refresh input data")
-    
+    parser.add_argument("-path", "--path", help="Path to the puzzle data", default="./tmp")
+
     args = parser.parse_args()
-    
+
     token = get_config_value("AOC_SESSION_TOKEN", args.token)
-        
-    client = Client(token, refresh=args.refresh, path="./tmp")
+
+    client = Client(token, refresh=args.refresh, path=args.path)
     solver = Solver(client)
 
     registration.register_all(solver)
-                
+
     solver.solve(args.year, args.day)
+
 
 if __name__ == "__main__":
     main()
