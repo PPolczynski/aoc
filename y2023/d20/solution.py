c14aa53d94b01a9ab80c94d3f19d2b86a6978bc4
pawel <pawel@polczynski.com>
2026-02-22 11:35:29 +0100

    year 2023 d20 part 2

--- y2023/d20/solution.py
+++ y2023/d20/solution.py
@@ -1,3 +1,4 @@
+import math
 import re
 from abc import abstractmethod
 from collections import deque
@@ -11,17 +12,24 @@ class Pulse(Enum):
     HIGH = 1
 
 
+class Notification(Enum):
+    ON_RECEIVE = 0
+    ON_TRANSMIT = 1
+
+
 class ModuleType(Enum):
     FLIPFLOP = "FLIP FLOP"
     BROADCASTER = "BROADCASTER"
     BUTTON = "BUTTON"
     CONJUNCTION = "CONJUNCTION"
     OUTPUT = "OUTPUT"
+    FINAL = "FINAL"
 
 
 _BROADCASTER_LABEL = "broadcaster"
 _BUTTON_LABEL = "button"
 _OUTPUT_LABEL = "output"
+_FINAL_LABEL = "rx"
 _FLIPFLOP_PREFIX = "%"
 _CONJUNCTION_PREFIX = "&"
 
@@ -33,6 +41,7 @@ class Module:
         self._type = module_type
         self.targets = targets
         self._transmitter.register(self)
+        self.senders = []
 
     @abstractmethod
     def receive(self, sender: str, pulse_tye: Pulse):
@@ -42,7 +51,10 @@ class Module:
         self._transmitter.transmit(self.label, target, pulse)
 
     def consume_announcement(self, sender: str):
-        return
+        self.senders.append(sender)
+
+    def notify(self, action: Notification, target, sender: str, pulse: Pulse):
+        pass
 
 
 class FlipFlop(Module):
@@ -94,6 +106,7 @@ class Conjunction(Module):
             self._send(target, pulse_to_send)
 
     def consume_announcement(self, sender: str):
+        super().consume_announcement(sender)
         self._memory[sender] = Pulse.LOW
 
 
@@ -106,25 +119,51 @@ class Output(Module):
         return
 
 
+class Final(Module):
+    def __init__(self, label: str, _: list[str], transmitter: "Transmitter"):
+        super().__init__(label, ModuleType.FINAL, [], transmitter)
+        self.peer_senders_cycle_track = dict()
+        self.all_high_recorded = False
+
+    def receive(self, sender: str, pulse_type: Pulse):
+        # print(f"output {self.label} received {pulse_type} from {sender}")
+        return
+
+    def consume_announcement(self, sender: str):
+        super().consume_announcement(sender)
+        self._transmitter.subscribe(self.label, sender, Notification.ON_RECEIVE)
+
+    def notify(self, action: Notification, target, sender: str, pulse: Pulse):
+        if pulse == Pulse.HIGH:
+            if sender not in self.peer_senders_cycle_track:
+                self.peer_senders_cycle_track[sender] = self._transmitter.cycle
+            # print(f"{str(action)} {sender} {str(pulse)}-> {target} cycle {self._transmitter.cycle}")
+        all_sender_senders = sum([len(self._transmitter.modules[sender].senders) for sender in self.senders])
+        if all_sender_senders == len(self.peer_senders_cycle_track):
+            self.all_high_recorded = True
+
+
 class Transmitter:
     def __init__(self):
-        self._modules = dict()
+        self.modules = dict()
         self.queue = deque()
         self.lock = False
         self.low_cnt = 0
         self.high_cnt = 0
+        self._on_receive = dict()
+        self.cycle = 0
 
     def register(self, module: Module):
-        self._modules[module.label] = module
+        self.modules[module.label] = module
 
     def announce_senders(self):
         missing_targets = []
-        for module in self._modules.values():
+        for module in self.modules.values():
             for target in module.targets:
-                if target not in self._modules:
+                if target not in self.modules:
                     missing_targets.append(target)
                     continue
-                self._modules[target].consume_announcement(module.label)
+                self.modules[target].consume_announcement(module.label)
         for target in missing_targets:
             Output(target, [], self)
 
@@ -132,18 +171,31 @@ class Transmitter:
         self.queue.append((sender, target, pulse))
         self._transmit()
 
+    def subscribe(self, me: str, target, notify_on: Notification):
+        if notify_on == Notification.ON_RECEIVE:
+            if target not in self._on_receive:
+                self._on_receive[target] = set()
+            self._on_receive[target].add(me)
+        else:
+            raise NotImplementedError(f"subscribe: {Notification.ON_RECEIVE} not implemented")
+
     def _transmit(self):
         if self.lock:
             return
         self.lock = True
+        self.cycle += 1
         while len(self.queue):
             sender, target, pulse = self.queue.popleft()
             # print(f"{sender} {str(pulse)} -> {target}")
-            self._modules[target].receive(sender, pulse)
+            self.modules[target].receive(sender, pulse)
             if pulse == Pulse.LOW:
                 self.low_cnt += 1
             else:
                 self.high_cnt += 1
+            if target in self._on_receive:
+                for listeners in self._on_receive[target]:
+                    self.modules[listeners].notify(Notification.ON_RECEIVE, target, sender, pulse)
+
         self.lock = False
 
 
@@ -174,8 +226,15 @@ def _part1(transmitter: Transmitter) -> any:
     return transmitter.low_cnt * transmitter.high_cnt
 
 
-def _part2(document: list[str]) -> any:
-    pass
+def _part2(transmitter: Transmitter) -> any:
+    button = Button(_BUTTON_LABEL, [_BROADCASTER_LABEL], transmitter)
+    final = Final(_FINAL_LABEL, [], transmitter)
+    transmitter.announce_senders()
+    while True:
+        button.send()
+        if final.all_high_recorded:
+            break
+    return math.lcm(*final.peer_senders_cycle_track.values())
 
 
 solution = Solution(
