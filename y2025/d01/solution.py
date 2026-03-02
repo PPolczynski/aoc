a1c7cdf2cf0a8659fc60b5d002f8b2f55f5474a4
pawel <pawel@polczynski.com>
2026-03-02 22:27:39 +0100

    day 1 part 2 year 2025

--- y2025/d01/solution.py
+++ y2025/d01/solution.py
@@ -7,32 +7,47 @@ _direction_to_int = {"L": -1, "R": 1}
 _range_end = 99
 _range_start = 0
 _start_position = 50
+_target = 0
 
 
 @dataclass
 class Rotation:
     direction: int
     clicks: int
+    full_rotations: int
 
     @classmethod
     def from_line(cls, line: str) -> 'Rotation':
-        direction, clicks = _direction_to_int[line[:1]], int(line[1:])
+        direction, clicks, full_rotations = _direction_to_int[line[:1]], int(line[1:]), 0
         if clicks > _range_end:
-            times = clicks // _range_end
+            full_rotations = clicks // _range_end
             clicks %= _range_end
-            clicks -= times  # because of extra click between 99 and 0
+            clicks -= full_rotations  # because of extra click between 99 and 0
         if clicks < 0:
+            full_rotations -= 1 # extra clicks between 99 0 has division to be one extra
             clicks += _range_end + 1  # +1 because 99 to 0 is extra click that was not taken
-        return Rotation(direction, clicks)
+        return Rotation(direction, clicks, full_rotations)
 
-    def position_after(self, position: int) -> int:
+    def rotate_from(self, position: int) -> tuple[int, bool]:
         next_position = position + self.direction * self.clicks
+        passed = False
         if next_position > _range_end:
             next_position = next_position - _range_end - 1
+            passed = True
         elif next_position < _range_start:
             next_position = _range_end + next_position + 1
-        return next_position
+            passed = True
+        return next_position, passed
 
+    def rotate_from_with_pass_cnt(self, position: int) -> [int, int]:
+        starts_at_target = position == _target
+        passes_from_clicks = 0
+        next_position, passed = self.rotate_from(position)
+        if next_position == _target and self.clicks != 0:
+            passes_from_clicks = 1
+        elif passed and not starts_at_target:
+            passes_from_clicks = 1
+        return next_position, self.full_rotations + passes_from_clicks
 
 def _preprocess(data: str) -> list[Rotation]:
     return [Rotation.from_line(line) for line in data.splitlines()]
@@ -42,14 +57,19 @@ def _part1(rotations: list[Rotation]) -> int:
     position = _start_position
     cnt = 0
     for rotation in rotations:
-        position = rotation.position_after(position)
-        if position == 0:
+        position, _ = rotation.rotate_from(position)
+        if position == _target:
             cnt += 1
     return cnt
 
 
 def _part2(rotations: list[Rotation]) -> any:
-    pass
+    position = _start_position
+    cnt = 0
+    for rotation in rotations:
+        position, passes = rotation.rotate_from_with_pass_cnt(position)
+        cnt += passes
+    return cnt
 
 
 solution = Solution("Secret Entrance", "1", "2025", part1=_part1, part2=_part2, part1_preprocess=_preprocess,
--- y2025/d01/test_solution.py
+++ y2025/d01/test_solution.py
@@ -5,32 +5,36 @@ from y2025.d01.solution import Rotation
 
 class TestRotation(TestCase):
     def test_from_line(self):
-        self.assertEqual(Rotation(-1, 68), Rotation.from_line("L68"))
-        self.assertEqual(Rotation(1, 48), Rotation.from_line("R48"))
-        self.assertEqual(Rotation(-1, 1), Rotation.from_line("L1"))
-        self.assertEqual(Rotation(-1, 0), Rotation.from_line("L100"))
-        self.assertEqual(Rotation(1, 0), Rotation.from_line("R100"))
-        self.assertEqual(Rotation(-1, 1), Rotation.from_line("L101"))
-        self.assertEqual(Rotation(-1, 99), Rotation.from_line("L199"))
+        self.assertEqual(Rotation(-1, 68, 0), Rotation.from_line("L68"))
+        self.assertEqual(Rotation(1, 48, 0), Rotation.from_line("R48"))
+        self.assertEqual(Rotation(-1, 1, 0), Rotation.from_line("L1"))
+        self.assertEqual(Rotation(-1, 0, 1), Rotation.from_line("L100"))
+        self.assertEqual(Rotation(1, 0, 1), Rotation.from_line("R100"))
+        self.assertEqual(Rotation(-1, 1, 1), Rotation.from_line("L101"))
+        self.assertEqual(Rotation(-1, 99, 1), Rotation.from_line("L199"))
 
     def test_position_after(self):
-        position = 50
-        self.assertEqual(82, Rotation.from_line("L68").position_after(position))
-        position = 82
-        self.assertEqual(52, Rotation.from_line("L30").position_after(position))
-        position = 52
-        self.assertEqual(0, Rotation.from_line("R48").position_after(position))
-        position = 0
-        self.assertEqual(95, Rotation.from_line("L5").position_after(position))
-        position = 95
-        self.assertEqual(55, Rotation.from_line("R60").position_after(position))
-        position = 55
-        self.assertEqual(0, Rotation.from_line("L55").position_after(position))
-        position = 0
-        self.assertEqual(99, Rotation.from_line("L1").position_after(position))
-        position = 99
-        self.assertEqual(0, Rotation.from_line("L99").position_after(position))
-        position = 0
-        self.assertEqual(14, Rotation.from_line("R14").position_after(position))
-        position = 14
-        self.assertEqual(32, Rotation.from_line("L82").position_after(position))
+        self.assertEqual((82, True), Rotation.from_line("L68").rotate_from(50))
+        self.assertEqual((52, False), Rotation.from_line("L30").rotate_from(82))
+        self.assertEqual((0, True), Rotation.from_line("R48").rotate_from(52))
+        self.assertEqual((95, True), Rotation.from_line("L5").rotate_from(0))
+        self.assertEqual((55, True), Rotation.from_line("R60").rotate_from(95))
+        self.assertEqual((0, False), Rotation.from_line("L55").rotate_from(55))
+        self.assertEqual((99, True), Rotation.from_line("L1").rotate_from(0))
+        self.assertEqual((0, False), Rotation.from_line("L99").rotate_from(99))
+        self.assertEqual((14, False), Rotation.from_line("R14").rotate_from(0))
+        self.assertEqual((32, True), Rotation.from_line("L82").rotate_from(14))
+
+    def test_rotate_from_with_pass_cnt(self):
+        self.assertEqual((82, 1), Rotation.from_line("L68").rotate_from_with_pass_cnt(50))
+        self.assertEqual((52, 0), Rotation.from_line("L30").rotate_from_with_pass_cnt(82))
+        self.assertEqual((0, 1), Rotation.from_line("R48").rotate_from_with_pass_cnt(52))
+        self.assertEqual((95, 0), Rotation.from_line("L5").rotate_from_with_pass_cnt(0))
+        self.assertEqual((55, 1), Rotation.from_line("R60").rotate_from_with_pass_cnt(95))
+        self.assertEqual((0, 1), Rotation.from_line("L55").rotate_from_with_pass_cnt(55))
+        self.assertEqual((99, 0), Rotation.from_line("L1").rotate_from_with_pass_cnt(0))
+        self.assertEqual((0, 1), Rotation.from_line("L99").rotate_from_with_pass_cnt(99))
+        self.assertEqual((14, 0), Rotation.from_line("R14").rotate_from_with_pass_cnt(0))
+        self.assertEqual((32, 1), Rotation.from_line("L82").rotate_from_with_pass_cnt(14))
+        # non example
+        self.assertEqual((50, 10), Rotation.from_line("R1000").rotate_from_with_pass_cnt(50))
