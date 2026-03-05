0a3164ccadb2c00990042e20e321118943e8ca68
pawel <pawel@polczynski.com>
2026-03-05 22:08:28 +0100

    day 3 year 2025 part 2

--- y2025/d03/solution.py
+++ y2025/d03/solution.py
@@ -17,12 +17,24 @@ def max_subsequence_value(numbers: list[int]) -> int:
     return first * 10 + second
 
 
+def max_subsequence_of_length_value(numbers: list[int], length: int) -> int:
+    start, end, value = 0, len(numbers) - length, 0
+    for _ in range(length):
+        max_v, max_v_idx = 0, 0
+        for idx in range(start, end + 1):
+            if numbers[idx] > max_v:
+                max_v, max_v_idx = numbers[idx], idx
+        start, end = max_v_idx + 1, end + 1
+        value = value * 10 + max_v
+    return value
+
+
 def _part1(banks: list[list[int]]) -> int:
     return sum([max_subsequence_value(bank) for bank in banks])
 
 
 def _part2(banks: list[list[int]]) -> int:
-    return 0
+    return sum([max_subsequence_of_length_value(bank, 12) for bank in banks])
 
 
 solution = Solution("Lobby ", "3", "2025",
--- y2025/d03/test_solution.py
+++ y2025/d03/test_solution.py
@@ -1,15 +1,29 @@
 from unittest import TestCase
 
-from y2025.d03.solution import max_subsequence_value
+from y2025.d03.solution import max_subsequence_value, max_subsequence_of_length_value
+
+
+def str_to_list_int(s: str) -> list[int]:
+    return [int(c) for c in s[:]]
 
 
 class Test(TestCase):
     def test_max_subsequence_value(self):
-        def str_to_list_int(s: str) -> list[int]:
-            return [int(c) for c in s[:]]
-
         self.assertEqual(98, max_subsequence_value(str_to_list_int("987654321111111")))
         self.assertEqual(89, max_subsequence_value(str_to_list_int("811111111111119")))
         self.assertEqual(78, max_subsequence_value(str_to_list_int("234234234234278")))
         self.assertEqual(92, max_subsequence_value(str_to_list_int("818181911112111")))
         self.assertEqual(22, max_subsequence_value(str_to_list_int("22")))
+
+    def test_max_subsequence_of_length_value(self):
+        # 2
+        self.assertEqual(98, max_subsequence_of_length_value(str_to_list_int("987654321111111"), 2))
+        self.assertEqual(89, max_subsequence_of_length_value(str_to_list_int("811111111111119"), 2))
+        self.assertEqual(78, max_subsequence_of_length_value(str_to_list_int("234234234234278"), 2))
+        self.assertEqual(92, max_subsequence_of_length_value(str_to_list_int("818181911112111"), 2))
+        self.assertEqual(22, max_subsequence_of_length_value(str_to_list_int("22"), 2))
+        # 12
+        self.assertEqual(987654321111, max_subsequence_of_length_value(str_to_list_int("987654321111111"), 12))
+        self.assertEqual(811111111119, max_subsequence_of_length_value(str_to_list_int("811111111111119"), 12))
+        self.assertEqual(434234234278, max_subsequence_of_length_value(str_to_list_int("234234234234278"), 12))
+        self.assertEqual(888911112111, max_subsequence_of_length_value(str_to_list_int("818181911112111"), 12))
