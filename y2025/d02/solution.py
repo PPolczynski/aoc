af4201ebad325ee5a62b9d6a12774c211d187ffd
pawel <pawel@polczynski.com>
2026-03-05 20:58:59 +0100

    day 2 year 2025 part 2

--- y2025/d02/solution.py
+++ y2025/d02/solution.py
@@ -1,11 +1,24 @@
+import math
+
 from puzzle import Solution
 from utils.range import Range
+
+
+def number_of_digits(n: int) -> int:
+    if n < 0:
+        # returns number of digits not characters
+        # this is very much not needed for this case
+        return int(math.log10(-n)) + 1
+    elif n == 0:
+        return 1
+    return int(math.log10(n)) + 1
 
 
 class ProductRange(Range):
     def __init__(self, start: str, end: str):
         super().__init__(int(start), int(end))
         self.start_str = start
+        self.end_str = end
 
     def get_invalid_ids(self) -> list[int]:
         out = []
@@ -32,6 +45,33 @@ class ProductRange(Range):
                 break
         return out
 
+    def get_all_invalid_ids(self) -> list[int]:
+        out = set()
+        l_start = len(self.start_str) if self.start > 9 else 2  # no repeats before 9
+        l_end = len(self.end_str)
+        for l in range(l_start, l_end + 1):
+            stop_l = l // 2 + 1
+            base = 1
+            while True:
+                n = number_of_digits(base)
+                if n == stop_l:
+                    break
+                elif l % n != 0:
+                    base = 10 ** n
+                    continue
+                times = l // n
+                candidate = 0
+                for _ in range(times):
+                    candidate *= 10 ** n
+                    candidate += base
+                if candidate > self.end:
+                    base = 10 ** n
+                    continue
+                elif self.contains_value(candidate):
+                    out.add(candidate)
+                base += 1
+        return list(out)
+
     @classmethod
     def from_string(cls, entry: str) -> "ProductRange":
         start, end = entry.split("-")
@@ -52,8 +92,11 @@ def _part1(product_ranges: list[ProductRange]) -> int:
     return out
 
 
-def _part2(rotations: list[ProductRange]) -> int:
-    pass
+def _part2(product_ranges: list[ProductRange]) -> int:
+    out = 0
+    for product_range in product_ranges:
+        out += sum(product_range.get_all_invalid_ids())
+    return out
 
 
 solution = Solution("Gift Shop", "2", "2025", part1=_part1, part2=_part2, part1_preprocess=_preprocess,
--- y2025/d02/test_solution.py
+++ y2025/d02/test_solution.py
@@ -16,3 +16,23 @@ class TestProductRange(TestCase):
         self.assertListEqual([11], ProductRange.from_string("11-11").get_invalid_ids())
         self.assertListEqual([], ProductRange.from_string("1-1").get_invalid_ids())
         self.assertListEqual([22, 33], ProductRange.from_string("16-33").get_invalid_ids())
+
+    def test_get_all_ids_within(self):
+        def lists_equal_without_order(want, got):
+            self.assertListEqual(sorted(want), sorted(got))
+
+        lists_equal_without_order([11, 22], ProductRange.from_string("11-22").get_all_invalid_ids())
+        lists_equal_without_order([99, 111], ProductRange.from_string("95-115").get_all_invalid_ids())
+        lists_equal_without_order([999, 1010], ProductRange.from_string("998-1012").get_all_invalid_ids())
+        lists_equal_without_order([1188511885], ProductRange.from_string("1188511880-1188511890").get_all_invalid_ids())
+        lists_equal_without_order([222222], ProductRange.from_string("222220-222224").get_all_invalid_ids())
+        lists_equal_without_order([], ProductRange.from_string("1698522-1698528").get_all_invalid_ids())
+        lists_equal_without_order([446446], ProductRange.from_string("446443-446449").get_all_invalid_ids())
+        lists_equal_without_order([38593859], ProductRange.from_string("38593856-38593862").get_all_invalid_ids())
+        lists_equal_without_order([565656], ProductRange.from_string("565653-565659").get_all_invalid_ids())
+        lists_equal_without_order([824824824], ProductRange.from_string("824824821-824824827").get_all_invalid_ids())
+        lists_equal_without_order([2121212121], ProductRange.from_string("2121212118-2121212124").get_all_invalid_ids())
+        lists_equal_without_order([11], ProductRange.from_string("11-11").get_all_invalid_ids())
+        lists_equal_without_order([], ProductRange.from_string("1-1").get_all_invalid_ids())
+        lists_equal_without_order([22, 33], ProductRange.from_string("16-33").get_all_invalid_ids())
+        lists_equal_without_order([11, 22, 33], ProductRange.from_string("1-33").get_all_invalid_ids())
