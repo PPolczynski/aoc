a810896a052bfcf24d5e86cdcef713d51aef20d4
pawel <pawel@polczynski.com>
2026-03-06 20:59:35 +0100

    day 4 year 2025

---
+++ y2025/d04/solution.py
@@ -0,0 +1,44 @@
+from puzzle import Solution
+from utils.matrix import Matrix
+
+_ROLL = "@"
+_EMPTY = "."
+_max_neighbours = 4
+
+
+def _preprocess(data: str) -> Matrix:
+    return Matrix(data.splitlines(), _EMPTY)
+
+
+def _part1(grid: Matrix) -> int:
+    accessible = 0
+    for v, c in grid:
+        if v == _ROLL:
+            cnt = len(list(filter(lambda x: x[0] == _ROLL, grid.adjacent(c, include_diagonal=True))))
+            if cnt < _max_neighbours:
+                accessible += 1
+    return accessible
+
+
+def _part2(grid: Matrix) -> int:
+    removed = 0
+    while True:
+        to_remove = set()
+        for v, c in grid:
+            if v == _ROLL:
+                neighbour_rolls = list(filter(lambda x: x[0] == _ROLL, grid.adjacent(c, include_diagonal=True)))
+                if len(neighbour_rolls) < _max_neighbours:
+                    to_remove.add(c)
+        if len(to_remove) == 0:
+            break
+        else:
+            removed += len(to_remove)
+            grid.apply(list(to_remove), _EMPTY)
+    return removed
+
+
+solution = Solution("Printing Department ", "4", "2025",
+                    part1=_part1,
+                    part2=_part2,
+                    part1_preprocess=_preprocess,
+                    part2_preprocess=_preprocess)
--- y2025/year.py
+++ y2025/year.py
@@ -2,5 +2,6 @@ from puzzle import Event
 from .d01 import solution as day1
 from .d02 import solution as day2
 from .d03 import solution as day3
+from .d04 import solution as day4
 
-event = Event([day1.solution, day2.solution, day3.solution, ])
+event = Event([day1.solution, day2.solution, day3.solution, day4.solution])
