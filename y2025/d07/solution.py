e3247a6455c753c959e19f7cf9e23c8537b980b4
pawel <pawel@polczynski.com>
2026-03-12 19:22:04 +0100

    day 7 year 2025 - touch up

--- y2025/d07/solution.py
+++ y2025/d07/solution.py
@@ -1,7 +1,6 @@
 from puzzle import Solution
 from utils.matrix import Matrix
 
-
 EMPTY = "."
 START = "S"
 SPLITTER = "^"
@@ -9,12 +8,13 @@ SPLITTER = "^"
 MOVE_DOWN = (0, 1)
 SPLIT_AND_MOVE_DOWN = [(-1, 1), (1, 1)]
 
+
 def _preprocess(data: str):
     lines = []
     for i, line in enumerate(data.splitlines()):
         if i == 0 or line.find(SPLITTER) != -1:
             lines.append(line)
-    return Matrix(lines, ".")
+    return Matrix(lines, EMPTY)
 
 
 def _part1(diagram: Matrix) -> int:
@@ -35,11 +35,12 @@ def _part1(diagram: Matrix) -> int:
         beams = tmp
     return splits
 
+
 def _part2(diagram: Matrix) -> int:
     mem = dict()
     start = diagram.find_first(START)
 
-    def dfs(beam: tuple[int,int]) -> int:
+    def dfs(beam: tuple[int, int]) -> int:
         if diagram.is_out_of_bounds(beam):
             return 0
         elif beam in mem:
@@ -52,7 +53,9 @@ def _part2(diagram: Matrix) -> int:
         else:
             mem[beam] = dfs((beam[0] + MOVE_DOWN[0], beam[1] + MOVE_DOWN[1]))
         return mem[beam]
+
     return 1 + dfs(start)
+
 
 solution = Solution("Laboratories", "7", "2025",
                     part1=_part1,
